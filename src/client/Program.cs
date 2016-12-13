81934490e57f1310744a0aec5c4ba37c965b3ee8
César López-Natarén <cesarn@mindtouch.com>
2016-12-12 17:02:39 -0800

    Handle the exceptions from talking to the server to avoid OSX's exception handler UI

--- src/client/Program.cs
+++ src/client/Program.cs
@@ -15,23 +15,28 @@ public static class Program {
             Console.WriteLine("invalid format for port, use a number");
             return -1;
         }
-        var channel = new Channel($"127.0.0.1:{port}", ChannelCredentials.Insecure);
-        var client = new DictionaryService.DictionaryServiceClient(channel);
-        var callOptions = new CallOptions(deadline: DateTime.UtcNow.Add(TimeSpan.FromSeconds(3)));
+        try {
+            var channel = new Channel($"127.0.0.1:{port}", ChannelCredentials.Insecure);
+            var client = new DictionaryService.DictionaryServiceClient(channel);
+            var callOptions = new CallOptions(deadline: DateTime.UtcNow.Add(TimeSpan.FromSeconds(3)));
 
-        // Set key
-        var setRequest = new SetRequest { Key = "foo", Value = "bar" };
-        var setResponse = client.Set(setRequest, callOptions);
-        Console.WriteLine($"setRequest={setRequest}");
-        Console.WriteLine($"setResponse={setResponse}");
+            // Set key
+            var setRequest = new SetRequest { Key = "foo", Value = "bar" };
+            var setResponse = client.Set(setRequest, callOptions);
+            Console.WriteLine($"setRequest={setRequest}");
+            Console.WriteLine($"setResponse={setResponse}");
 
-        // Retrieve key
-        var getRequest = new GetRequest { Key = "foo" };
-        var getResponse = client.Get(getRequest, callOptions);
-        Console.WriteLine($"getRequest={getRequest}");
-        Console.WriteLine($"getResponse={getResponse}");
-        channel.ShutdownAsync().Wait();
-        Console.WriteLine("\ngRPC client exiting");
+            // Retrieve key
+            var getRequest = new GetRequest { Key = "foo" };
+            var getResponse = client.Get(getRequest, callOptions);
+            Console.WriteLine($"getRequest={getRequest}");
+            Console.WriteLine($"getResponse={getResponse}");
+            channel.ShutdownAsync().Wait();
+            Console.WriteLine("\nGRPC client exiting");
+        } catch (Exception e) {
+            Console.WriteLine($"There was an error: {e.Message}");
+            return -1;
+        }
         return 0;
     }
 }
